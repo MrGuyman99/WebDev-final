d1fd3f74aa0b2cbe995d77c09b3ded1e9823c323
MrGuyman99 <bodiepagelow@gmail.com>
2026-05-23 18:49:11 -0500

    Cleaned up search

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // URL -> https://data.cityofchicago.org/api/v3/views/xq83-jr8c/query.csv
 
 /*
-    Structure of .CSV file:
+    Structure of Chicago Energy Benchmarking .CSV file:
     Data Year, ID, Property Name, Reporting Status, Address,
     ZIP Code, Chicago Energy Rating, Exempt From Chicago Energy Rating
 */
@@ -33,6 +33,7 @@ function readCSV(file) {
 }
 
 // Iterates through the dataset stored in data and searches for a given term
+// We return the entire row so we can use whatever data we want from it
 function search(data, search_term) {
     for (const row of data) {
         if (row[2] == search_term) {
@@ -43,7 +44,8 @@ function search(data, search_term) {
 
 let data;
 // Made the function as async so we can use await and wait for the Promise function to finish
-document.getElementById('filepicker').addEventListener('change', async function (event) {
+// This runs when the file gets picked
+document.getElementById("filepicker").addEventListener("change", async function (event) {
     const file = event.target.files[0];
     if (!file) return;
     const content = await readCSV(file);
@@ -51,10 +53,18 @@ document.getElementById('filepicker').addEventListener('change', async function 
     data = content.slice(1);
 });
 
-document.getElementById('buttonSubmit').addEventListener('submit', function (e) {
+// This runs when the we press submit
+document.getElementById('filePicker').addEventListener('submit', function (e) {
+    // Prevents page from resetting
     e.preventDefault();
+    // Get the term to use the search function with and use global variables data
     let term = document.getElementById("searchTerm").value;
+    let search_result = search(data, term);
+    // Attempt to search with the given term
     if (term != "") {
-        document.getElementById('output').innerText += search(data, term);
+        document.getElementById("output").innerText = `Property name "${search_result[2]}"
+            at address ${search_result[4]} has an energy rating of ${search_result[6]}`;
+    } else {
+        alert("Please input something for a search term!");
     }
 });
