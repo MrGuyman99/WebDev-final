37d3935f170ccb7a0888a5a4a1b8c673a9be96d5
MrGuyman99 <bodiepagelow@gmail.com>
2026-05-27 18:27:40 -0500

    Multiple Search Results Displaying Properly

--- script.js
+++ script.js
@@ -46,13 +46,17 @@ let what_checked;
 // We return the entire row so we can use whatever data we want from it
 function search(data, search_term, search_type) {
     // We find what to search for (Zip Code, Address, e.t.c)
+    // The values of our radio buttons correspond to the indexes of the data we want to check
+    // E.X Checking for zipcodes -> what_checked = 5; data[5] would access zipcode
     what_checked = Number(iterateRadios(search_type).value);
+    let total = [];
     for (const row of data) {
         // All the values for all of the checkboxes corespond to indexes
         if (row[what_checked] == search_term) {
-            return [row, what_checked];
+            total.push(row);
         }
     }
+    return total;
 }
 
 let data;
@@ -71,15 +75,33 @@ document.getElementById('filePicker').addEventListener('submit', function (e) {
     // Prevents page from resetting
     e.preventDefault();
     let term = document.getElementById("searchTerm").value;
+    // Gets the result of our search function
+    // The search function iterates through the radios on its own 
     let search_result = search(data, term, document.getElementsByName("search_type"));
     let output = document.getElementById("output");
+    // Setting output to zero before doing anything (Repeat searches)
     output.innerText = "";
-    const [row, type] = search_result;
-    if (type === 2) {
-        output.innerText += `Property name "${row[2]}" at address ${row[4]} has an energy rating of ${row[6]}`;
-    } else if (type === 4 || type === 5) {
-        output.innerText += `Address "${row[4]}" in Zip-Code ${row[5]} has an energy rating of ${row[6]}`;
-    } else {
-        output.innerText += `Within zip code "${row[5]} at address ${row[4]} the energy rating is ${row[6]}`
+
+    // Runs if search results are empty
+    if (search_result.length === 0) {
+        output.innerText += `Search result turned up nothing, sorry man :(`;
+        return;
+    }
+
+    // We loop through all the search results and print out the corresponding message based on what the search type was
+    for (let i = 0; i < search_result.length; i++) {
+        // Property Name
+        if (what_checked === 2) {
+            output.innerText += `In ${search_result[i][0]} property name "${search_result[i][2]}" at address ${search_result[i][4]} has an energy rating of ${search_result[i][6]}\n`;
+        }
+        // Address + Zipcode
+        else if (what_checked === 4 || what_checked === 5) {
+            output.innerText += `In ${search_result[i][0]} address "${search_result[i][4]}" in Zip-Code ${search_result[i][5]} has an energy rating of ${search_result[i][6]}\n`;
+        }
+        // Energy level (Note: Looking up "0.0" would return 4160 entrances, may remove this later)
+        // Other side note: When I ran this firefox kindly asked that I close the tab and then firefox proceeded to crash
+        else {
+            output.innerText += `In ${search_result[i][0]} within zip code "${search_result[i][5]}" at address "${search_result[i][4]}" the energy rating is: ${search_result[i][6]}\n`;
+        }
     }
 });
